9afb225422f791ef8a22cb2151e605663ea3d11f
Shazia-Gul <shaziagul@postgrad.otago.ac.nz>
2022-04-30 09:00:34 +1200

    Updated code for Individual UserID and Phone Model

--- NodeJsCode/apifile.js
+++ NodeJsCode/apifile.js
@@ -3,13 +3,18 @@ const express = require('express')
 const app = express()
 const port = 3001;
 var rimraf = require("rimraf");
-
+const session = require('express-session');
 var fs = require("fs");
 var cron = require('node-cron');
+var lineReader = require('line-reader'); 
+const readline = require('readline');
 const path = require('path');
 var filecounter = 1;
-var foldername = "/home/shazia/";
-
+var userPath = "/home/shazia/";
+var connectedUserID="";
+var smartPhoneModel="";
+var sess;
+const param1='shazia';
 
 function rawBody(req, res, next) {
   var chunks = [];
@@ -32,41 +37,59 @@ function rawBody(req, res, next) {
   });
 }
 
-function RunCronJob() {
-  cron.schedule("*/1 * * * * *", () => {
-    console.log('Cron Job Runing-------\n ');
-    const { exec } = require('child_process');
-    exec("/home/shazia/ARscript.sh");
-  });
+app.post('/test', rawBody, function (req, res) {
+  
+      const rl = readline.createInterface({
+        input: fs.createReadStream('/Users/shazia-gul/poses_esac_.txt'),
+        crlfDelay: Infinity
+      });
+      
+      rl.on('line', (line) => {
 
-}
+        if (line.startsWith("65.jpg")){
+          console.log(`Display: ${line}`);
 
-function getRandomFileName() {
-  var timestamp = new Date().toISOString().replace(/[-:.]/g, "");
-  var random = ("" + Math.random()).substring(2, 8);
-  var random_number = timestamp + random;
-  return random_number;
-}
+        }
+       
+      });
+            
+          
+          
+    });
 
+ app.post('/send-PhoneModel', rawBody, function (req, res) {
+  if (req.rawBody && req.bodyLength > 0) {
+ 
+       smartPhoneModel = req.rawBody.toString();
+        console.log("At Phone Model"+connectedUserID);
+       
+       const data=fs.readFile(userPath+'MobCalib.txt', 'utf8', function read(err, data) { if (err) { throw err; } 
+      data = JSON.parse(data); 
+       for (var key in data) { 
+        console.log("User key"+data[0][smartPhoneModel]);
+        smartPhoneModel=data[0][smartPhoneModel];
+       
+       }
+     });
+     res.send('Your Phone Model is '+smartPhoneModel);
+   } else {
+     res.send(500);
+  }
+
+ });
+
+ app.post('/send-User', rawBody, function (req, res) {
+      //sess=req.session;
+      connectedUserID=req.rawBody.toString()
+     // sess.connectedUserID=req.rawBody.toString();
+      console.log(connectedUserID);
+ });
 
 function DelFolderData() {
 
-  //  if(fs.existsSync("/home/shazia/esac/datasets/fbs/"+"test"))
-  //      {
-  //         rimraf("/home/shazia/esac/datasets/fbs/"+"test", function () { console.log("done"); });
-  //     }    
-
-  //     CreateNewFolderIfNotExist("test");
-  //     CreateNewFolderIfNotExist("test/calibration");
-  //     CreateNewFolderIfNotExist("test/poses");
-  //     CreateNewFolderIfNotExist("test/rgb");
-
-  const subdir1 = foldername + "esac/datasets/fbs/test/rgb";
-  const subdir2 = foldername + "esac/datasets/fbs/test/poses";
-  const subdir3 = foldername + "esac/datasets/fbs/test/calibration";
-
-
-
+  const subdir1 = userPath + "esac/datasets/fbs/test_"+connectedUserID+"/rgb";
+  const subdir2 = userPath + "esac/datasets/fbs/test_"+connectedUserID+"/poses";
+  const subdir3 = userPath + "esac/datasets/fbs/test_"+connectedUserID+"/calibration";
 
   fs.readdir(subdir1, (err, files) => {
     if (err) throw err;
@@ -106,88 +129,61 @@ function DelFolderData() {
 }
 
 app.post('/createfolder', function (req, res) {
-  console.log("Create Folder API Called");
-  CreateNewFolderIfNotExist("Sallman");
- // DelFolderData();
- // console.log("Data Deleted Sucessfully");
+  
+     CreateNewFolderIfNotExist("test_"+connectedUserID);
+     CreateNewFolderIfNotExist("test_"+connectedUserID+"/calibration");
+     CreateNewFolderIfNotExist("test_"+connectedUserID+"/poses");
+     CreateNewFolderIfNotExist("test_"+connectedUserID+"/rgb");
+  //DelFolderData();
   res.status(200, { status: 'Folder Created Successfull' });
 });
 
 app.post('/upload-image', rawBody, function (req, res) {
-  //658.999
-  //3054.54 Iphone 12 focal length
 
- DelFolderData();
+ console.log("On Image Upload "+connectedUserID);
 
   if (req.rawBody && req.bodyLength > 0) {
 
-
-    const data=fs.readFileSync(foldername + "esac/datasets/fbs/test.txt", 'utf8' , (err, data) => {
-      if (err) {
-        console.error(data)
-        return
-      }
-      console.log(data)
-    })
-    
-    fs.writeFile(foldername + "esac/datasets/fbs/test/rgb/" + filecounter + ".jpg", req.rawBody, err => {
+    fs.writeFile(userPath + "esac/datasets/fbs/test_"+connectedUserID+"/rgb/" + filecounter + ".jpg", req.rawBody, err => {
       if (err) throw err;
-      filecounter++;
+     //filecounter++;
     })
-    fs.writeFile(foldername + "esac/datasets/fbs/test/calibration/" + filecounter + ".jpg" + ".calibration" + ".txt",data.toString(), err => {
+    fs.writeFile(userPath + "esac/datasets/fbs/test_"+connectedUserID+"/calibration/" + filecounter + ".jpg" + ".calibration" + ".txt",smartPhoneModel, err => {
       if (err) throw err;
 
     })
     array = "1 0 0 0\n0 1 0 0\n0 0 1 0\n0 0 0 1";
-    fs.writeFile(foldername + "esac/datasets/fbs/test/poses/" + filecounter + ".jpg" + ".poses" + ".txt", array, err => {
+    fs.writeFile(userPath + "esac/datasets/fbs/test_"+connectedUserID+"/poses/" + filecounter + ".jpg" + ".poses" + ".txt", array, err => {
       if (err) throw err;
 
     })
-
-    // TODO save image (req.rawBody) somewhere
-
-    // send some content as JSON
     res.send('Image Sent Successfully to the server');
-   // res.send(200, { status: 'Image Stored Successfully in the server' });
+   
   } else {
     res.send(500);
   }
 
 });
 
-function CreateNewFolderIfNotExist(foldername)
-{
+function CreateNewFolderIfNotExist(foldername){
+  console.log("At Folder"+connectedUserID);
   try {
-    if (!fs.existsSync("/home/shazia/esac/datasets/fbs/"+foldername)) {
-      fs.mkdirSync("/home/shazia/esac/datasets/fbs/"+foldername)
+    if (!fs.existsSync(userPath+"esac/datasets/fbs/"+foldername)) {
+      fs.mkdirSync(userPath+"esac/datasets/fbs/"+foldername)
     }
-
-   else if (fs.existsSync("/home/shazia/esac/datasets/fbs/"+foldername)) {console.log("FOlder Exist");}
+  
   }
   
-  
-  
-    catch (err) {
+   catch (err) {
     console.error(err)
   }
    
 }
 
-
-// TODO save image (req.rawBody) somewhere
-
-
 app.post('/runBatchFile', function (req, res) {
-  // const { exec } = require('child_process');
-  //  fs.unlinkSync(foldername+"esac/environments/fbs/poses_esac_.txt")
-
-  // exec( foldername+"ARscript.sh");
-  // console.log("Batch FIle");
-
-  // Child process is required to spawn any kind of asynchronous process
   var childProcess = require("child_process");
   // This line initiates bash
-  var script_process = childProcess.spawn('/bin/bash', ['/home/shazia/ARscript.sh'], { env: process.env });
+  var script_process = childProcess.exec(`"/home/shazia/ARscript.sh" "${connectedUserID}"`);
   // Echoes any command output 
   script_process.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
@@ -200,27 +196,61 @@ app.post('/runBatchFile', function (req, res) {
   // Process exit
   script_process.on('close', function (code) {
     console.log('child process exited with code ' + code);
-
-    fs.readFile(foldername + "esac/environments/fbs/poses_esac_.txt", function (err, data) {
-      res.end(data, { status: 'ESAC Executeeeeeeeeed Successfully' });
+    const rl = readline.createInterface({
+      input: fs.createReadStream(userPath + "esac/environments/fbs/poses_esac_"+connectedUserID+".txt"),
+      crlfDelay: Infinity
+    });
     
-      const { exec } = require('child_process');
-      exec(foldername + "GVisBatch.sh");
-      console.log("GVIS Batch File Executed");
+    rl.on('line', (line) => {
+
+//      console.log(`Display: ${line}`);
+      if (line.startsWith(filecounter+".jpg")){
+        filecounter++;
+        console.log(`Display: ${line}`);
+        res.send(`${line}`)
+        //return;
+
+      }
+     
+    });
+
+  //  fs.readFile(userPath + "esac/environments/fbs/poses_esac_"+connectedUserID+"+.txt", function (err, data) {
+   //   res.end(data, { status: 'ESAC Executeeeeeeeeed Successfully' });
+    
+   //   const { exec } = require('child_process');
+    //  exec(userPath+ "GVisBatch.sh");
+   //   console.log("GVIS Batch File Executed");
 
     });
 
   });
 
-});
-app.post('/runCronJob', function (req, res) {
-  RunCronJob();
-});
+
+
 app.get('/readPosesfile', function (req, res) {
-  fs.readFile(foldername + "esac/environments/fbs/poses_esac_.txt", function (err, data) {
-    res.send(data);
+  //fs.readFile(userPath + "esac/environments/fbs/poses_esac_"+connectedUserID+".txt", function (err, data) {
+  //  res.send(data);
+
+    const rl = readline.createInterface({
+      input: fs.createReadStream(userPath + "esac/environments/fbs/poses_esac_"+connectedUserID+".txt"),
+      crlfDelay: Infinity
+    });
+    
+    rl.on('line', (line) => {
+      console.log(filecounter+"filecounter");
+      console.log(`Display: ${line}`);
+
+      if (line.startsWith(filecounter+".jpg")){
+        filecounter++;
+        
+        console.log(`Display: ${line}`);
+        res.send(`${line}`);
+
+      }
+     
+    });
   });
-});
+
 app.listen(port, () => {
   console.log(`Server app listening at http://localhost:${port}`)
 })
